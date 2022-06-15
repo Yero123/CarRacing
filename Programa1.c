937cd72ebc250d7a3969347478fdacb03adf0688
Adrian <adanhuaman2011@gmail.com>
2022-06-15 02:52:01 -0500

    LogicGame.h

--- Programa1.c
+++ Programa1.c
@@ -24,6 +24,8 @@
 #include "Timer.h"
 #include "LogicGame.h"
 
+
+
 struct bala{
 	int isCrashed;
 	int isVisible;
@@ -36,26 +38,46 @@ bala3={0,0,30,30};
 
 struct enemigo{
 	int isCrashed;
+	int isVisible;
 	int x;
 	int y;
 }
-enemigo1={0,80,47},
-enemigo2={0,30,30},
-enemigo3={0,30,30};
+enemigo1={0,0,64,47},
+enemigo2={0,0,64,29},
+enemigo3={0,0,64,8};
 
-
+int count=0;
 struct carro{
 	int x;
 	int y;
 }
 carro={2,47};
 
-
+int aleatorio=0;
+int aleatorio_2 = 0;
+int aleatorio_3 = 0;
+int i= 0;
 int tiempo=0;
 int disparar=0;
 
+
+
 void SysTick_Handler(void){
 	tiempo++;
+	i++;
+	if(i%11 == 0){
+		srand(tiempo);
+	}
+	aleatorio=rand()%3;
+	if(tiempo%11 == 0){
+		srand(i+4);
+	}
+	aleatorio_2=rand()%3;
+	if(i%6 == 0){
+		srand(i+10);
+	}
+	aleatorio_3=rand()%3;
+	aleatorio = (aleatorio + aleatorio_2 + aleatorio_3)%3;
 	if(tiempo%500==0){
 		if(bala1.isVisible){
 			bala1.x=bala1.x+10;
@@ -65,16 +87,58 @@ void SysTick_Handler(void){
 			bala1.isVisible=0;
 		}
 	}
-	if(tiempo>=1000){
-		if(enemigo1.x<30){
-			enemigo1.x=80;
+	if(i >=739){
+		i = 0;
+	}
+	if(tiempo>=100){
+		if(enemigo1.x<20){
+			enemigo1.isVisible=0;
 		}else{
 		enemigo1.x=enemigo1.x-10;
 		}
+		if(enemigo2.x<20){
+			enemigo2.isVisible=0;
+		}else{
+		enemigo2.x=enemigo2.x-10;
+		}
+		if(enemigo3.x<20){
+			enemigo3.isVisible=0;
+		}else{
+		enemigo3.x=enemigo3.x-10;
+		}
 		tiempo=0;
+		count++;
+		if(count==4){
+			count=0;
+		}
 	}
-	
+if(count==3){
+	  count = 0;
+		if(aleatorio==0 && !enemigo1.isVisible){
+			enemigo1.isVisible=1;
+			enemigo1.x=64;
+		}
+		if(aleatorio==1 && !enemigo2.isVisible){
+			enemigo2.isVisible=1;
+			enemigo2.x=64;
+		}
+		if(aleatorio==2 && !enemigo3.isVisible){
+			enemigo3.isVisible=1;
+			enemigo3.x=64;
+		}
+	}
+
 }
+
+
+void evaluarDisparo(){
+	char lectura2 = UART0_DR_R;
+	if(lectura2 == 't'){
+		bala1.isVisible=1;
+		bala1.y=carro.y;
+	}
+}
+
 
 int main(void)
 {
@@ -89,13 +153,16 @@ int main(void)
 	setName(nickName);
 	setLevel(&level);
 	Nokia5110_DisplayBuffer();
-	/*Juego*/
-	while(1){
 	
+	while(1){
+		
+	  //evaluarDisparo();
 		Nokia5110_ClearBuffer();
 		evaluarMovimiento(&carro.y);
 		Nokia5110_PrintBMP(carro.x, carro.y, Car, 2);
-		Nokia5110_PrintBMP(enemigo1.x,enemigo1.y, Enemigo, 2);
+		if(enemigo1.isVisible)Nokia5110_PrintBMP(enemigo1.x,enemigo1.y, Enemigo, 2);
+		if(enemigo2.isVisible)Nokia5110_PrintBMP(enemigo2.x,enemigo2.y, Enemigo, 2);
+		if(enemigo3.isVisible)Nokia5110_PrintBMP(enemigo3.x,enemigo3.y, Enemigo, 2);
 		if(bala1.isVisible)Nokia5110_PrintBMP(bala1.x, bala1.y, Bala, 2);
 		Nokia5110_DisplayBuffer();
 		Nokia5110_SetCursor(0,1);
